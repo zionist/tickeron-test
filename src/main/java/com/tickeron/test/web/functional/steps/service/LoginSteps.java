8e950b6e944e5875d7e6457cdd76418b82e56148
slaviann <slaviann@gmail.com>
2015-10-13 13:21:54 +0300

    cleanup old LoginSteps code

--- src/main/java/com/tickeron/test/web/functional/steps/service/LoginSteps.java
+++ src/main/java/com/tickeron/test/web/functional/steps/service/LoginSteps.java
@@ -11,26 +11,4 @@ import static org.junit.Assert.*;
  */
 public class LoginSteps extends  ServiceStepsBasic {
 
-    @When("I make login with user $user and password $password")
-    public void login(String username, String password) throws InterruptedException {
-        username = paramsAndVariablesSteps.substituteParamsAndVariables(username);
-        password = paramsAndVariablesSteps.substituteParamsAndVariables(password);
-        getWebDriver().get(environment.getProperty("server.url"));
-        sleepBigTimeout();
-        // Login button
-        getWebDriver().findElement(By.cssSelector(".cx-marketing-menu-login-button")).click();
-        sleepSmallTimeout();
-        getWebDriver().findElement(By.cssSelector("#email")).sendKeys(username);
-        getWebDriver().findElement(By.cssSelector("#password")).sendKeys(password);
-        getWebDriver().findElement(By.cssSelector("input.btn")).click();
-    }
-
-    @Then("The login is successful")
-    public void CheckLoginIsSuccessfull() {
-        sleepBigTimeout();
-        // Check welcome div. Must be displayed
-        WebElement welcomeDiv = getWebDriver().findElement(By.cssSelector(".jspPane > div:nth-child(1)"));
-        assertEquals(welcomeDiv.getText(), "Welcome to Tickeron. Please select one of the options to the right; " +
-                "and we will teach you how to use the system and guide you through basic steps.");
-    }
 }
